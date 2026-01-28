71b8784f513e98a6f87d7c86349d852ac35d6b5a
JairGZZ <jairprox1234@gmail.com>
2026-01-28 17:49:08 -0500

    refactor: Delete unused repository boilerplate and `RepositoryBase` interface, introduce `ActionsRepository` and `TestController`.

--- api/src/database/drizzle.schema.ts
+++ api/src/database/drizzle.schema.ts
@@ -34,8 +34,7 @@ export const promotions = pgTable('promotions', {
   id: uuid('id').defaultRandom().primaryKey(),
   name: text('name').notNull(),
   description: text('description').notNull(),
-  promotion_type: integer('promotion_type')
-    .references(() => promotionTypes.id),
+  promotion_type: integer('promotion_type').references(() => promotionTypes.id),
   start_date: timestamp('start_date').notNull(),
   end_date: timestamp('end_date').notNull(),
   created_at: timestamp('created_at').notNull().defaultNow(),
@@ -60,19 +59,3 @@ export const promotionActions = pgTable('promotion_actions', {
   action_type: promotionActionsTypesEnum('action_type').notNull(),
   configuration: jsonb('configuration').notNull(),
 });
-
-//select
-export type PromotionActionSelect = InferSelectModel<typeof promotionActions>;
-export type PromotionConditionSelect = InferSelectModel<
-  typeof promotionConditions
->;
-export type PromotionTypeSelect = InferSelectModel<typeof promotionTypes>;
-export type PromotionSelect = InferSelectModel<typeof promotions>;
-
-// insert
-export type PromotionInsert = InferInsertModel<typeof promotions>;
-export type PromotionConditionInsert = InferInsertModel<
-  typeof promotionConditions
->;
-export type PromotionActionInsert = InferInsertModel<typeof promotionActions>;
-export type PromotionTypeInsert = InferInsertModel<typeof promotionTypes>;
--- api/src/interfaces/RepositoryBase.ts
+++
@@ -1,7 +0,0 @@
-export interface RepositoryBase<TSelect, TInsert> {
-  create(data: TInsert): Promise<TSelect>;
-  update(id: string, data: TInsert): Promise<TSelect>;
-  delete(id: string): Promise<void>;
-  findById(id: string): Promise<TSelect | null>;
-  findAll(): Promise<TSelect[]>;
-}
