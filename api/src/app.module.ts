500d75c807e7c8f82288d0ccede51aee8431d8bb
Iv44n <ivandev2oo6@gmail.com>
2026-01-28 12:47:44 -0500

    feat: Remove boilerplate `AppController` and `AppService`, configure `AppModule` with `ConfigModule` and `DrizzleModule`, and introduce `PromotionsService`.

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DrizzleModule } from './database/drizzle.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DrizzleModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
