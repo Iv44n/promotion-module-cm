5e46d8d9c268619da418cc974b7bb8cf091eca5c
Iv44n <ivandev2oo6@gmail.com>
2026-01-28 16:49:42 -0500

    feat: Implement global HTTP and Zod exception filters and add a promotion creation endpoint.

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './exceptions/http-exception.filter';
+import { ZodExceptionFilter } from './exceptions/zod-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new GlobalExceptionFilter(), new ZodExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
