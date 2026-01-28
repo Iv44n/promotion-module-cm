d3e14bd7e1f08ee9a38f0fab49b517e91b520956
Iv44n <ivandev2oo6@gmail.com>
2026-01-28 16:16:43 -0500

    fix: format the code

--- api/src/dto/response/PromotionResponseDTO.ts
+++ api/src/dto/response/PromotionResponseDTO.ts
@@ -1,12 +1,12 @@
 import z from 'zod';
 
 export const PromotionResponseDTOSchema = z.object({
-    id: z.uuid(),
-    name: z.string(),
-    description: z.string().optional(),
-    isActive: z.boolean(),
-    startDate: z.date(),
-    endDate: z.date(),
+  id: z.uuid(),
+  name: z.string(),
+  description: z.string().optional(),
+  isActive: z.boolean(),
+  startDate: z.date(),
+  endDate: z.date(),
 });
 
 export type PromotionResponseDTO = z.infer<typeof PromotionResponseDTOSchema>;
